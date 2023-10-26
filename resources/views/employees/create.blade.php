e3dc35f523c62c0c9b8123854b06dbf858355918
Filip Kloska <fkloska1@gmail.com>
2023-10-26 21:23:10 +0200

    change

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -64,15 +64,6 @@
                 <div class="mb-6">
                     <div id="dropdownSearch" class="z-10  hidden bg-white rounded-lg shadow w-60 ">
                         <div class="p-3">
-                            <label for="input-group-search" class="sr-only">Search</label>
-                            <div class="relative">
-                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                    <svg class="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                    </svg>
-                                </div>
-                                <input type="text" id="input-group-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 " placeholder="Search">
-                            </div>
                         </div>
                         <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700" aria-labelledby="dropdownSearchButton">
                             @foreach ($dietaryPreferences as $preference)
